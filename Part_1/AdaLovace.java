959bd91788fa795db7cf252e0e933173009922ee
Sebastian David Ibañez Rios <98895197+sebdavid3@users.noreply.github.com>
2024-11-07 01:03:15 -0500

    Add files via upload

---
+++ Part_1/AdaLovace.java
@@ -0,0 +1,7 @@
+package Part_1;
+
+public class AdaLovace {
+    public static void main(String[] args){
+        System.out.println("Ada Lovelace");
+    }
+}
---
+++ Part_1/HiAdaLovelace.java
@@ -0,0 +1,9 @@
+package Part_1;
+
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        System.out.println("Hi " + name);
+    }
+}
